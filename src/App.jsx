db2d0ac45d18a6dbdc614d19826471d664d4929e
sugonhwang <sugonhwang1@gmail.com>
2025-11-07 19:53:25 +0900

    ✨ 홈페이지, 무비스 페이지 이동 기능 구현 및 페이지 스타일링

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import AppLayout from "./layout/AppLayout";
+import Homepage from "./pages/Homepage/Homepage";
+import MoviePage from "./pages/Movies/MoviePage";
+import MovieDetailPage from "./pages/MovieDetail/MovieDetailPage";
+import NotFoundPage from "./pages/NotFoundpage/NotFoundPage";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+// 홈페이지 /
+// 영화 전체 보여주는 페이지(서치 기능 포함) /movie
+// 영화 디테일 페이지 /movies/:id
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <Routes>
+        <Route path="/" element={<AppLayout />}>
+          <Route index element={<Homepage />} />
+          <Route path="movies">
+            <Route index element={<MoviePage />} />
+            <Route path=":id" element={<MovieDetailPage />} />
+          </Route>
+        </Route>
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/Movies/MoviePage.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MoviePage = () => {
+  return <div>Movie</div>;
+};
+
+export default MoviePage;
